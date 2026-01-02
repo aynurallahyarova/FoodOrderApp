d4901806bd632b22f8f8782168a754ed4af743b5
Aynur Allahyarova <aynurallahyarova5@gmail.com>
2026-01-02 12:32:35 +0400

    projectCreated

---
+++ FoodOrderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodOrderApp
+//
+//  Created by Aynur on 02.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
