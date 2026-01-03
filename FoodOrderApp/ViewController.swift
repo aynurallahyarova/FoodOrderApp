a1139bb4c1008fa87c5656857faf93d0b229a074
Aynur Allahyarova <aynurallahyarova5@gmail.com>
2026-01-03 14:10:33 +0400

    signInSignUp button

--- FoodOrderApp/ViewController.swift
+++ FoodOrderApp/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var animationView: UIView!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewDidLayoutSubviews()
+
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        containerView.layer.cornerRadius = containerView.frame.height / 2
+        containerView.clipsToBounds = true
+  
+        animationView.layer.cornerRadius = animationView.frame.height / 2
+        animationView.clipsToBounds = true
     }
 
-
+    
+    
+    private func configureSegment(isSignInTapped: Bool) {
+        leadingConstraint.constant = isSignInTapped ? 0 : 150
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.signInButton.setTitleColor(isSignInTapped ? .white : .red, for: .normal)
+            self.signUpButton.setTitleColor(isSignInTapped ? .red : .white, for: .normal)
+        }
+        
+        
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        configureSegment(isSignInTapped: true)
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+        configureSegment(isSignInTapped: false)
+    }
 }
 
