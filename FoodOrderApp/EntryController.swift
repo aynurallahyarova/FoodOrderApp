81cdf152978eebb3abfc2f67f1f9ab5ebf11ab15
Aynur Allahyarova <aynurallahyarova5@gmail.com>
2026-01-03 15:10:32 +0400

    resultButton

--- FoodOrderApp/EntryController.swift
+++ FoodOrderApp/EntryController.swift
@@ -7,43 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class EntryController: UIViewController {
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
-    
+    @IBOutlet weak var resultButton: UIButton!
+    @IBOutlet weak var confirmePasswordText: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var animationView: UIView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewDidLayoutSubviews()
 
-        
+        configureUI()
     }
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        containerView.layer.cornerRadius = containerView.frame.height / 2
-        containerView.clipsToBounds = true
-  
-        animationView.layer.cornerRadius = animationView.frame.height / 2
-        animationView.clipsToBounds = true
-    }
-
     
+    private func configureUI() {
+        containerView.clipsToBounds = true
+        containerView.layer.cornerRadius = containerView.frame.height / 2
+  
+        animationView.clipsToBounds = true
+        animationView.layer.cornerRadius = animationView.frame.height / 2
+        
+        resultButton.clipsToBounds = true
+        resultButton.layer.cornerRadius = animationView.frame.height / 2
+    }
     
     private func configureSegment(isSignInTapped: Bool) {
+        confirmePasswordText.isHidden = isSignInTapped
         leadingConstraint.constant = isSignInTapped ? 0 : 150
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
         } completion: { _ in
             self.signInButton.setTitleColor(isSignInTapped ? .white : .red, for: .normal)
             self.signUpButton.setTitleColor(isSignInTapped ? .red : .white, for: .normal)
+            self.resultButton.setTitle(isSignInTapped ? "Sign in" : "Sign up", for: .normal)
         }
-        
-        
     }
-    
+
     @IBAction func signInTapped(_ sender: Any) {
         configureSegment(isSignInTapped: true)
     }
@@ -51,5 +54,8 @@ class ViewController: UIViewController {
     @IBAction func signUpTapped(_ sender: Any) {
         configureSegment(isSignInTapped: false)
     }
+    
+    @IBAction func resultButtonTapped(_ sender: Any) {
+        
+    }
 }
-
