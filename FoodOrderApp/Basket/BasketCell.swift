785cb36ca57f323aa9e285058578db18d47bd45f
Aynur Allahyarova <aynurallahyarova5@gmail.com>
2026-01-17 14:57:02 +0400

    basket

---
+++ FoodOrderApp/Basket/BasketCell.swift
@@ -0,0 +1,12 @@
+//
+//  BasketCell.swift
+//  FoodOrderApp
+//
+//  Created by Aynur on 17.01.26.
+//
+
+import UIKit
+
+class BasketCell: UICollectionViewCell {
+    
+}
--- FoodOrderApp/Controller/BasketController.swift
+++ FoodOrderApp/Controller/BasketController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class BasketController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var collection: UICollectionView!
+    let viewModel = BasketViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
+        collection.dataSource = self
+        collection.delegate = self
 
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collection.reloadData()
+        viewModel.fetchItems()
+        upDateTotal()
+    }
+    private func upDateTotal() {
+        totalLabel.text = "Total: \(viewModel.totalPrice)$"
+    }
+}
+extension BasketController: UICollectionViewDataSource, UICollectionViewDelegate {
 
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return viewModel.itemCount()
+    }
 
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath)
+        -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "BasketCell",
+            for: indexPath
+        ) as! BasketCell
+
+        let item = viewModel.item(at: indexPath.item)
+
+        return cell
+    }
 }
